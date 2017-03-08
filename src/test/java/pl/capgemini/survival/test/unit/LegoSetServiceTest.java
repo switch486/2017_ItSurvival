966dd048ffb38224044bbbf4dddd99199a8dd09f
Adam Puchalski <adam.puchalski@capgemini.com>
2017-03-08 09:39:42 +0100

    3.0.step

--- src/test/java/pl/capgemini/survival/test/unit/LegoSetServiceTest.java
+++ src/test/java/pl/capgemini/survival/test/unit/LegoSetServiceTest.java
@@ -1,10 +1,5 @@
 package pl.capgemini.survival.test.unit;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.when;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +7,6 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import pl.capgemini.survival.common.exception.ValidationException;
-import pl.capgemini.survival.common.typ.Condition;
-import pl.capgemini.survival.common.typ.Status;
 import pl.capgemini.survival.persistence.entity.LegoSet;
 import pl.capgemini.survival.persistence.repo.LegoSetRepository;
 import pl.capgemini.survival.service.LegoSetService;
@@ -34,26 +27,7 @@ public class LegoSetServiceTest {
 
 	@Test
 	public void shouldCreateLegoSet() {
-		// given
-		LegoSet legoSet = new LegoSet();
-		legoSet.setExternallId("123");
-		legoSet.setLegoComment("comment");
-		legoSet.setLegoCondition(Condition.NEW);
-		legoSet.setLegoName("StarTrek");
-		legoSet.setLegoStatus(Status.OWNED);
-		legoSet.setLegoVersion(0);
-
-		LegoSet newLegoSet = new LegoSet();
-		newLegoSet.setId(5L);
-
-		when(legoSetRepository.save(legoSet)).thenReturn(newLegoSet);
-
-		// when
-		LegoSet createdLegoSet = legoSetService.createLegoSet(legoSet);
-
-		// then
-		assertThat(createdLegoSet, notNullValue());
-		assertThat(createdLegoSet.getId(), equalTo(5L));
+		throw new RuntimeException("Not Implemented");
 	}
 
 	@Test(expected = ValidationException.class)
